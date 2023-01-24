6bce5feac9873b0407fdcaa92c279eff8f15b5fc
GIORGIO AMATI <g.amati@cineca.it>
2023-01-24 19:45:01 +0100

    add drag e lift files

---
+++ SRC/drag.F90
@@ -0,0 +1,87 @@
+!=====================================================================
+!     ****** LBE/drag
+!
+!     COPYRIGHT
+!       (c) 2000-2011 by CASPUR/G.Amati
+!       (c) 2013-20?? by CINECA/G.Amati
+!     NAME
+!       bcond
+!     DESCRIPTION
+!       computing drag according Gauss Theorem
+!     INPUTS
+!       itime
+!       delta
+!       iunit
+!     OUTPUT
+!       drag
+!     TODO
+!       
+!     NOTES
+!        the center and the radius of the cylinder are hardwritten
+!
+!     *****
+!=====================================================================
+!
+        subroutine drag(itime,delta,iunit)
+!
+        use storage
+        use timing
+        implicit none
+!
+        integer:: itime
+        integer:: iunit
+        integer:: istart,istop
+        integer:: jstart,jstop
+        integer:: icoord, jcoord, delta
+!
+        real(mykind) :: fluxX, fluxY
+!
+! Drag section (still to fix)
+!
+! cylinder center
+        icoord = int(2.0*l/5.0)
+        jcoord = int(m/2.0)
+! 
+! cylinder radius           
+        if (m.gt.256) then
+           radius = 32
+        else
+           radius = ly/8
+        endif
+!
+! Gauss Th bounding box
+        istart = icoord - radius - delta
+        istop  = icoord + radius + delta
+        jstart = jcoord - radius - delta
+        jstop  = jcoord + radius + delta
+!           
+        fluxX = zero
+        fluxY = zero
+!           
+        call fluxX_y(itime,istart,istop,jstart,jstop,fluxX)
+        call fluxX_x(itime,istart,istop,jstart,jstop,fluxY)
+        write(iunit,*) itime, fluxX, fluxY
+!
+        call flush(iunit)            ! flush for drag
+!
+! stop timing
+        call time(tcountA1)
+        call SYSTEM_CLOCK(countA1, count_rate, count_max)
+        time_dg = time_dg + real(countA1-countA0)/(count_rate)
+        time_dg1 = time_dg1 + (tcountA1-tcountA0)
+!
+!
+#ifdef DEBUG_2
+        if(myrank == 0) then
+              write(6,*) "DEBUG2: Exiting from sub. drag", itime
+!              write(6,*) "DEBUG2", int(2.0*l/5.0),int(m/2.0)
+!              write(6,*) "DEBUG2", icoord, jcoord
+!              write(6,*) "DEBUG2", radius, delta
+!              write(6,*) "DEBUG2", istart, istop 
+!              write(6,*) "DEBUG2", jstart, jstop 
+!              write(6,*) "DEBUG2", fluxX, fluxY
+        endif
+#endif
+!
+        return
+        end subroutine drag
