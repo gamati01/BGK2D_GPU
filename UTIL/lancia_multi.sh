af15b6acdb9f1b47e4a6bdfc4464eb5024155691
GIORGIO AMATI <gamati01@login14.leonardo.local>
2023-07-19 09:37:46 +0200

    improved script

--- UTIL/lancia_multi.sh
+++ UTIL/lancia_multi.sh
@@ -2,7 +2,7 @@ for ver in doconcurrent offload openacc; do
     echo $ver
     mkdir $ver
     cd $ver
-    for size in bgk.2048.R10000.input bgk.2912.R10000.input; do
+    for size in bgk.256.R100.input bgk.512.R1000.input bgk.1024.R10000.input bgk.2048.R10000.input bgk.2912.R10000.input ; do
 	echo $size
 	mkdir $size
 	cd $size
