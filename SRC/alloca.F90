12bf32d59365d1fd24ee7d89a66de7e079952424
GIORGIO AMATI <gamati01@login01.leonardo.local>
2023-08-29 14:20:37 +0200

    clean alloca.F90

--- SRC/alloca.F90
+++ SRC/alloca.F90
@@ -17,37 +17,36 @@
 !
 !     *****
 !=====================================================================
-subroutine alloca()
+!
+      subroutine alloca()
+!            
       use storage; 
       use timing
 
-#ifdef CUDAFOR
-      use cudafor
-#endif
 ! 
       implicit none
 !
 !     define unit vector  (real/integer)
 !
-       cx(  1    ) =  1
-       cx(  2    ) =  1
-       cx(  3    ) =  1
-       cx(  4    ) =  1
-       cx(  5    ) =  1
-       cx(  6    ) =  0
-       cx(  7    ) =  0
-       cx(  8    ) =  0
-       cx(  9    ) =  0
-       cx( 10    ) = -1
-       cx( 11    ) = -1
-       cx( 12    ) = -1
-       cx( 13    ) = -1
-       cx( 14    ) = -1
-       cx( 15    ) =  0
-       cx( 16    ) =  0
-       cx( 17    ) =  0
-       cx( 18    ) =  0
-       cx( 19    ) =  0
+      cx(  1    ) =  1
+      cx(  2    ) =  1
+      cx(  3    ) =  1
+      cx(  4    ) =  1
+      cx(  5    ) =  1
+      cx(  6    ) =  0
+      cx(  7    ) =  0
+      cx(  8    ) =  0
+      cx(  9    ) =  0
+      cx( 10    ) = -1
+      cx( 11    ) = -1
+      cx( 12    ) = -1
+      cx( 13    ) = -1
+      cx( 14    ) = -1
+      cx( 15    ) =  0
+      cx( 16    ) =  0
+      cx( 17    ) =  0
+      cx( 18    ) =  0
+      cx( 19    ) =  0
 
       icx(  1    ) =  1
       icx(  2    ) =  1
@@ -69,25 +68,25 @@ subroutine alloca()
       icx( 18    ) =  0
       icx( 19    ) =  0
 !
-       cy(  1    ) = -1
-       cy(  2    ) =  0
-       cy(  3    ) =  1
-       cy(  4    ) =  0
-       cy(  5    ) =  0
-       cy(  6    ) =  0
-       cy(  7    ) =  1
-       cy(  8    ) =  1
-       cy(  9    ) =  1
-       cy( 10    ) = -1
-       cy( 11    ) =  0
-       cy( 12    ) =  1
-       cy( 13    ) =  0
-       cy( 14    ) =  0
-       cy( 15    ) =  0
-       cy( 16    ) = -1
-       cy( 17    ) = -1
-       cy( 18    ) = -1
-       cy( 19    ) =  0
+      cy(  1    ) = -1
+      cy(  2    ) =  0
+      cy(  3    ) =  1
+      cy(  4    ) =  0
+      cy(  5    ) =  0
+      cy(  6    ) =  0
+      cy(  7    ) =  1
+      cy(  8    ) =  1
+      cy(  9    ) =  1
+      cy( 10    ) = -1
+      cy( 11    ) =  0
+      cy( 12    ) =  1
+      cy( 13    ) =  0
+      cy( 14    ) =  0
+      cy( 15    ) =  0
+      cy( 16    ) = -1
+      cy( 17    ) = -1
+      cy( 18    ) = -1
+      cy( 19    ) =  0
 !      
       icy(  1    ) = -1
       icy(  2    ) =  0
@@ -171,17 +170,17 @@ subroutine alloca()
 #endif
 
 #ifdef DEBUG_1
-        if(myrank == 0) then
-           write(6,*) "DEBUG1: Exiting from sub. alloca"
-        endif
+      if(myrank == 0) then
+         write(6,*) "DEBUG1: Exiting from sub. alloca"
+      endif
 #endif
 !
 # ifdef MEM_CHECK
-        if(myrank == 0) then
-           mem_stop = get_mem();
-           write(6,*) "MEM_CHECK: after sub. alloca mem =", mem_stop
-        endif
+      if(myrank == 0) then
+         mem_stop = get_mem();
+         write(6,*) "MEM_CHECK: after sub. alloca mem =", mem_stop
+      endif
 # endif
 
-end subroutine alloca
+      end subroutine alloca
 
