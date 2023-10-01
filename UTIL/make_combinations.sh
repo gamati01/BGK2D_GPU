bb7b8a54f3eadd19f8a1db0e6e7014875dc2ed69
Giorgio Amati <g.amati@cineca.it>
2023-10-01 19:20:36 +0200

    Improved Makefile + list of configurations

---
+++ UTIL/make_combinations.sh
@@ -0,0 +1,18 @@
+#	
+# loop over targets
+for target in `cat target.list`; do 
+	# loop over compilers
+	for comp in `cat compiler.list`; do 
+		# loop over implementations
+		for imp in `cat implementation.list`; do 
+                	# loop over precision
+		        for prec in `cat precision.list`; do 
+                        	# loop over testcase.list
+				for test in `cat testcase.list`; do 
+					echo "make " $comp $target $imp $prec $test
+				done
+			done
+		done
+	done
+done
+
