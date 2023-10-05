fe29802370ae6cc106f50610c135fa0203dfbf6e
GIORGIO AMATI <gamati01@login01.leonardo.local>
2023-10-05 11:53:47 +0200

    fix

---
+++ TEST/VonKarmanStreet/runMe.1.sh
@@ -0,0 +1,16 @@
+echo "VK stress for different paradigm"
+echo "------------------------------"
+echo "run #1 of 3: Do concurrent i  "
+sleep 5
+./run.single.gpu.fused.dc.nv.x
+echo "------------------------------"
+echo "run #2 of 3: openACC          "
+sleep 5
+./run.single.gpu.fused.openacc.nv.x
+echo "------------------------------"
+echo "run #3 of 3: OMP offload      "
+sleep 5 
+./run.single.gpu.fused.offload.nv.x
+echo "------------------------------"
+echo "to plot: use gnuplot and load 'plot.gnu'"
+
