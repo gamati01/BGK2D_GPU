5c3bb4520d8130841494e4174528d72f936cfd13
Giorgio Amati <g.amati@cineca.it>
2023-10-24 20:23:18 +0200

    added CI

---
+++ CI/make_test.sh
@@ -0,0 +1,14 @@
+#clean
+export numTest=`grep -v CEST Make_OK.txt | wc -l`
+grep -v CEST Make_OK.txt > test.lst
+echo $numTest
+for test in `seq 1 $numTest`; do
+	opt=`head -n $test test.lst | tail -n 1`
+	echo $test $opt
+	cd ../SRC
+	make clean
+	make $opt VER=try
+	cd -
+	cp ../RUN/bgk2d.try.x .
+	./bgk2d.try.x > output.$test.log
+done
