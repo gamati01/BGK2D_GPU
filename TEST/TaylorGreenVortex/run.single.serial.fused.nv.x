d7e9197eeecf44d9f96c721669b530bf2d44d45f
GIORGIO AMATI <g.amati@cineca.it>
2023-10-30 16:51:29 +0100

    fix some uncorrect issues

---
+++ TEST/TaylorGreenVortex/run.single.serial.fused.nv.x
@@ -0,0 +1,44 @@
+#!/bin/tcsh
+#
+setenv DIR RUN_SINGLE_SERIAL_FUSED_NV
+setenv EXE bgk2d.serial.x
+#
+echo "---------------------------"
+echo "starting test TG vortices  "
+echo " ---> nvfortran            "
+echo " ---> single precision     "
+echo " ---> fused                "
+echo " ---> " $EXE
+echo " ---> " $DIR
+echo "---------------------------"
+#
+rm -r $DIR
+mkdir $DIR
+cd $DIR
+
+# step 1: compiling
+echo "step 1: compiling"
+cd ../../../SRC
+make clean
+make serial NVIDIA=1 SINGLE=1 FUSED=1 TGV=1
+if ($?) then
+   echo "compiling fails..."
+   exit 1
+else
+   cd -
+   cp ../../../RUN/$EXE  .
+   cp ../bgk.input.small bgk.input
+   echo "compiling  ended succesfully..."
+endif
+
+
+# step 2: running test
+echo "step 2: running test"
+./$EXE >& out.log
+if (-e "bgk.perf") then
+   echo "run ended succesfully..."
+else
+   echo "running test fails..."
+   exit 1
+endif
+
--- TEST/TaylorGreenVortex/runMe.2.sh
+++ TEST/TaylorGreenVortex/runMe.2.sh
@@ -1,16 +1,20 @@
 echo "TG for different precision"
 echo "------------------------------"
-echo "run #1 of 3: single precision "
+echo "run #1 of 4: single precision "
 sleep 5
-./run.single.serial.fused.gnu.x
+./run.mixed1.serial.fused.nvidia.x
 echo "------------------------------"
-echo "run #2 of 3: mixed (single/double) precision "
-sleep 5 
-./run.mixed2.serial.fused.gnu.x
+echo "run #2 of 4: single precision "
+sleep 5
+./run.single.serial.fused.nvidia.x
 echo "------------------------------"
-echo "run #3 of 3: double precision "
+echo "run #3 of 4: mixed (single/double) precision "
 sleep 5 
-./run.double.serial.fused.gnu.x
+./run.mixed2.serial.fused.nvidia.x
+echo "------------------------------"
+echo "run #4 of 4: double precision "
+sleep 5 
+./run.double.serial.fused.nvidia.x
 echo "------------------------------"
 echo "to plot: use gnuplot and load 'plot.small.gnu'"
 
