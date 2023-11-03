22fb5946b45d1a2ed8fdef05cfde210b907cf1f0
agiorgio <agiorgio@login5.head.adastra.cines.fr>
2023-11-03 14:54:55 +0100

    Added set-up for Adastra

---
+++ UTIL/setup.adastra.sh
@@ -0,0 +1,17 @@
+#
+module purge
+module load craype-x86-trento
+module load libfabric/1.15.2.0
+module load craype-network-ofi
+module load perftools-base/23.02.0
+module load cce/15.0.1
+module load craype/2.7.21
+module load cray-dsmml/0.2.2
+module load cray-mpich/8.1.24
+module load cray-libsci/23.05.1.4
+module load PrgEnv-cray/8.3.3
+module load craype-accel-amd-gfx90a
+module load rocm/5.3.0
+module li
+#
+
